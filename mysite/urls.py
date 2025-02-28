ca5fccead8fd950f204cd00f8d9d48b5cdb37b29
Abdulaziz Hamidjonov <163741823+abdulaziz-python@users.noreply.github.com>
2025-02-28 14:08:44 +0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.urls import re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 
